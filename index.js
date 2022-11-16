08ccfbe99852e08ed82bea8b7a7599d73789f30b
Cesar Carrasco Jr <Dad@Marthas-iMac.local>
2022-11-15 23:35:23 -0800

    customized this trivia game

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your Code Here
+let userName = window.prompt ('Please enter your name:')
+    console.log(userName, ", welcome to my trivia game!")
+    let userScore = 0
+    let playAgain = true
+
+function playGame() {   
+    for (let i = 0; i < questions.length; i++) {
+            let question = questions [i]
+            let userAnswer = window.prompt(question.text, "Enter the Capitalized Letter of your Selection")
+            console.log("question #", i + 1)    
+            console.log("The answer is:", userAnswer)
+
+                    if (userAnswer === question.correctAnswer) {
+                        console.log('Correct')
+                        userScore = userScore +10  
+                        } else {
+                            console.log('false')
+                        }
+
+
+    }
+    window.alert ('Your score is: ' + userScore)
+    if (userScore < 70) {
+        window.alert ("loser! You scored less than 70 points. You should try again")
+        } else {
+            window.alert ("Winner! Winner! Chicken dinner!  You scored above 70.  You passed.")
+        }  
+    
+
+}
+
+while (playAgain === true) {
+    playGame()
+            let userChoice = window.prompt('Would you like to play the game again? Answer yes or no.')
+
+                if (userChoice === 'yes') {
+                        playAgain = true
+                    } else {
+                        playAgain = false
+                        window.alert ('Thanks for playing the game!')
+                    }
+
+
+}
